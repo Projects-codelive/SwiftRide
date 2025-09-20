4557c9cd6948fb8953d14af1eebcb2085d39b3fb
Projects-codelive <sg790540@gmail.com>
2025-09-21 00:17:33 +0530

    Added All New Functionalities

--- Backend/server.js
+++ Backend/server.js
@@ -11,13 +11,13 @@ const io = initializeSocket(server);
 // Make io accessible in the app for controllers
 app.set('io', io);
 
-// server.listen(port, () => {
-//     console.log(`Server is running on port ${port}`);
-//     console.log(`Socket.IO server initialized`);
-// });
-
-server.listen(port, '0.0.0.0', () => {
+server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
     console.log(`Socket.IO server initialized`);
-    console.log(`Server accessible on all network interfaces`);
 });
+
+// server.listen(port, '0.0.0.0', () => {
+//     console.log(`Server is running on port ${port}`);
+//     console.log(`Socket.IO server initialized`);
+//     console.log(`Server accessible on all network interfaces`);
+// });
